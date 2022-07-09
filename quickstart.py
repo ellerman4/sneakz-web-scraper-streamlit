22a13ec0a13e545da433cf227219fe56b5cffdf1
ellerman <106990217+ellerman4@users.noreply.github.com>
2022-07-09 15:21:07 -0500

    add css animation to button

--- quickstart.py
+++ quickstart.py
@@ -12,6 +12,76 @@ invalid_id = RuntimeError('You may have entered an invalid SteamID')
 
 st.set_page_config(layout="wide")
 
+st.markdown(
+"""
+<style>
+.css-wq85zr {
+    display: inline-flex;
+    -webkit-box-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    justify-content: center;
+    font-weight: 400;
+    padding: 0.25rem 0.75rem;
+    border-radius: 0.25rem;
+    margin: 0px;
+    border-radius: 10px;
+    line-height: 1.6;
+    color: inherit;
+    width: auto;
+    user-select: none;
+    background-color: rgb(43, 44, 54);
+    border: 1px solid rgba(250, 250, 250, 0.2);
+    width: 304px;
+}
+.css-wq85zr:before {
+    content: '';
+    background: linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000);
+    position: absolute;
+    top: -2px;
+    left:-2px;
+    background-size: 400%;
+    z-index: -1;
+    filter: blur(5px);
+    width: calc(100% + 4px);
+    height: calc(100% + 4px);
+    animation: glowing 20s linear infinite;
+    opacity: 0;
+    transition: opacity .3s ease-in-out;
+    border-radius: 10px;
+}
+.css-wq85zr:active {
+    color: #000
+}
+.css-wq85zr:active:after {
+    background: transparent;
+}
+.css-wq85zr:hover:before {
+    opacity: 1;
+}
+
+.css-wq85zr:after {
+    z-index: -1;
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #111;
+    left: 0;
+    top: 0;
+    border-radius: 10px;
+}
+
+@keyframes glowing {
+    0% { background-position: 0 0; }
+    50% { background-position: 400% 0; }
+    100% { background-position: 0 0; }
+}
+</style>
+""",
+unsafe_allow_html=True
+)
+
 # Build a sidebar for user input
 with st.sidebar:
     st.title("Sneakz web scraper")
